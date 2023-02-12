e65518897c50bcd37b2fac657bd9a8e232b60d23
Juan S. Marquerie <juansebastian.marquerie01@estudiant.upf.edu>
2023-02-12 17:02:08 +0100

    Added room logic no redis for this

--- game_logic.js
+++ game_logic.js
@@ -1,68 +1,72 @@
 var GAME_SERVER_MANAGER = {
 
-    init: function(on_finish) {
-      var redis = require('redis');
-      this.redis_client = redis.createClient();
+  init: function() {
+    this.rooms = {};
+    this.active_users = {};
 
-      this.redis_client.connect().then(on_finish).catch(function(err) { console.log("fef", err); });
-      return this;
-    },
+    return this;
+  },
 
-    get_chatrooms: function (on_finish) {
-      this.redis_client.LRANGE('chat_rooms', 0, -1).then(function(v) {
-         on_finish(v);
-      });
-    },
+  room_template: {
+    users:[],
+    back_img: ''
+  },
 
-    create_chatroom: function(chatroom_name, on_finish) {
-      this.redis_client.LPUSH('chat_room', chatroom_name);
-      on_finish();
-    },
+  user_template: {
+    position: {x: 0, y: 0}
+  },
 
-    get_users_on_chatroom: function(room_id, on_finish) {
-      this.redis_client.LRANGE('chat_' + room_id + '_users', 0, -1).then(function(v) {
-         on_finish(v);
-      }).catch(function(error) {
-        console.log(error);
-        on_finish([]);
-      });
-    },
+  add_room: function(name, back_img) {
+    var room_obj = {... this.room_template};
 
-    get_users_on_chatroom_near_me: function(room_id, user_id, distance, on_finish) {
-      this.redis_client.LRANGE('chat_' + room_id + '_users', 0, -1).then(function(v) {
-        var user_pos = null;
-        var near_user_id_list = [];
-        // Find the current user's position
-        for(var i = 0; i < v.length; i++) {
-          if (v[i].id == user_id) {
-            user_pos = v[i].position.x;
-          }
-        }
+    room_obj.back_img = back_img;
 
-        for(var i = 0; i < v.length; i++) {
-          // Skip the current user
-          if (v[i].id == user_id) {
-            continue;
-          }
+    this.rooms[name] = room_obj;
+  },
 
-          // If its near, or less the max distance, add to the list
-          if (Math.abs(v[i].position.x - user_pos) < distance) {
-            near_user_id_list.push(v[i].id);
-          }
-        }
 
-        on_finish(near_user_id_list);
-      }).catch(function(error) {
-        console.log(error);
-        on_finish([]);
-      });
+  get_users_on_chatroom: function(room_id, on_finish) {
+    return this.rooms[room_id].users;
+  },
+
+  get_users_on_chatroom_near_me: function(room_id, user_id, distance, on_finish) {
+    var room_users = this.rooms[room_id].users;
+    var user_pos = null;
+    var near_user_id_list = [];
+    // Find the current user's position
+    for(var i = 0; i < room_users.length; i++) {
+      if (room_users[i] == user_id) {
+        user_pos = this.active_users[room_users[i]].position.x;
+      }
     }
 
-    join_chatroom: function(user_id, room_id, on_finish) {
-      this.redis_client.LPUSH('chat_' + room_id + '_users', user_id);
-      on_finish();
+    // Find the users near the user
+    for(var i = 0; i < room_users.length; i++) {
+      // Skip the current user
+      if (room_users[i].id == user_id) {
+        continue;
+      }
 
+      // If its near, or less the max distance, add to the list
+      if (Math.abs(room_users[i].position.x - user_pos) < distance) {
+        near_user_id_list.push(room_users[i].id);
+      }
     }
+
+    return near_user_id_list;
+  },
+
+  add_user: function(user_id) {
+    this.users[user_id] = {... self.user_template};
+  },
+
+  remove_user: function(user_id) {
+    delete this.users[user_id];
+  },
+
+  join_chatroom: function(user_id, room_id) {
+    this.rooms[room_id].users.push(user_id);
+  }
 };
 
 module.exports = {game_server: GAME_SERVER_MANAGER};
